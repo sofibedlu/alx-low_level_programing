483d229e98740e5ef54c420374f61baefba49c20
sofiB <bedlusofi@gmail.com>
2022-09-09 15:22:47 +0000

    test1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,8 @@ char charType;
 
 	float floatType;
 
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));	
-        printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 
