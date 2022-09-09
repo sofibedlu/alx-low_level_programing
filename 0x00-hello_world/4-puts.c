0c2b6464df3437dcb0e0ad05a005436bc39fdece
sofiB <bedlusofi@gmail.com>
2022-09-09 13:33:11 +0000

    puts-function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char my_char[] = "Programming is like building a multilingual puzzle";
+	puts(my_char);
+	return 0;
+}
