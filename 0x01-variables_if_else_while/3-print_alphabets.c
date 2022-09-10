325a957777974780136ef1ac03c4d4ed8c6f51f0
sofiB <bedlusofi@gmail.com>
2022-09-10 08:34:16 +0000

    small and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char chC = 'A';
+	
+	while(ch <= 'z'){
+		putchar(ch);
+		ch++;
+	}
+	while(chC <= 'Z'){
+		putchar(chC);
+		chC++;	
+	}
+	putchar('\n');
+	return (0);
+}
