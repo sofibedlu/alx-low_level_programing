0616f0a852fe937a1613c8a25b585d6272d8fbd8
sofiB <bedlusofi@gmail.com>
2022-09-10 09:34:35 +0000

    reverse-order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		--ch;
+	}
+	putchar('\n');
+	return (0);
+}
