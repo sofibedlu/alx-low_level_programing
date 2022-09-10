a22f97ebb85f5844eb8e5d91c4f00a980a4f90c7
sofiB <bedlusofi@gmail.com>
2022-09-10 09:24:22 +0000

    puchar only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
