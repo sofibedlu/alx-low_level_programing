c6dd1417efe40d44f6bafef3bdbeb8a120bd670a
sofiB <bedlusofi@gmail.com>
2022-09-15 18:35:40 +0000

    main

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return - always 0
+ */
+int main(void)
+{
+	printstring("_putchar");
+	_putchar('\n');
+	return (0);
+
+}
