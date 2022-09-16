c08feb7531e8b9f128d2901aa7791f13c22fc93a
sofiB <bedlusofi@gmail.com>
2022-09-16 20:21:29 +0000

    98989

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 			if (n != 98)
 				printf("%d, ", n);
 			else
-				printf("%d", n);
+				printf("%d ", n);
 			n++;
 		}
 	}
@@ -28,7 +28,7 @@ void print_to_98(int n)
 			if (n != 98)
 				printf("%d, ", n);
 			else
-				printf("%d", n);
+				printf("%d ", n);
 			n--;
 		}
 	}
