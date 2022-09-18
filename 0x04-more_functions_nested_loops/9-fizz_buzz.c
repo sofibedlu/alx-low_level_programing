a9bce06ab20dd3ab29f1db687510e75e9bb1c129
sofiB <bedlusofi@gmail.com>
2022-09-18 07:51:01 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, i3, i5;
+
+	i = 1;
+
+	while (i <= 100)
+	{
+		i3 = i / 3;
+		i5 = i / 5;
+
+		if (i == (i3 * 3) && i == (i5 * 5))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i == (i3 * 3))
+		{
+			printf("Fizz ");
+		}
+		else if (i == (i5 * 5))
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	return (0);
+}
