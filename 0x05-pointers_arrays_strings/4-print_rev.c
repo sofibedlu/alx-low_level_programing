77ae0e1cf1da663d5c312b962d3793d8441b6724
sofiB <bedlusofi@gmail.com>
2022-09-20 10:23:56 +0000

    reverse1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@ void print_rev(char *s)
 	int i;
 	int n = 0;
 
-	while (*s != '\0')
+	while (*s)
 	{
 		n++;
 		s++;
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
-		_putchar(*s);
+		_putchar(*(s - 1));
 		s--;
 	}
 	_putchar('\n');
