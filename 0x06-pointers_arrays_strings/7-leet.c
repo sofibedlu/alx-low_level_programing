ae9a7a4e9b14788dc0e3b415f13ed2ad58051232
sofiB <bedlusofi@gmail.com>
2022-09-22 17:26:58 +0000

    leet22

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,22 +9,19 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char A[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	int n[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-		else
-			continue;
+		for (j = 0; n[j] != '\0'; j++)
+		{
+			if (s[i] == A[j] || s[i] == A[j + 5])
+			{
+				s[i] = n[j];
+			}
+		}
 	}
 	return (s);
 }
