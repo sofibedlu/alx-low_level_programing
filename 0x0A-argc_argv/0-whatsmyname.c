3cf7bbeac16849f2e892ca2f06f9833e1f04c62f
sofi <bedlusofi@gmail.com>
2022-09-30 11:47:43 +0000

    name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - return the name of the program
+ * @argc: array size
+ * @argv: pointer of array
+ * Return: 0 - success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+argc and argv
+command line argument
+how to use arguments passed to your program
