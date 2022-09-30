13aec835f79cb5ef4baa871098e1d412cafa93ea
sofi <bedlusofi@gmail.com>
2022-09-30 11:56:11 +0000

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print number of argument passed into it
+ * @argc: array size
+ * @argv: pointer to string
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
