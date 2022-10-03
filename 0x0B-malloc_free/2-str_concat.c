0c4c45af29c9031b6d60f6619d06e079676e497a
sofi <bedlusofi@gmail.com>
2022-10-03 20:20:44 +0000

    correcton-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,8 +18,10 @@ char *str_concat(char *s1, char *s2)
 	int x, y;
 
 	i = 0;
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	x = strlen(s1);
 	y = strlen(s2);
 	s = malloc(x + y + 1);
