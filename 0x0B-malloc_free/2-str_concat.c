8bf5e35a6ee5e309b66fb6d73e2008850c82a76b
sofi <bedlusofi@gmail.com>
2022-10-03 20:01:25 +0000

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * str_concat - concatenates two string
+ * @s1: pointer 1
+ * @s2: pointer 2
+ * Return: string pointer or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i;
+	int x, y;
+
+	i = 0;
+	x = strlen(s1);
+	y = strlen(s2);
+	s = malloc(x + y + 1);
+	if (s != NULL)
+	{
+		while (s1[i] != '\0')
+		{
+			s[i] = s1[i];
+			i++;
+		}
+		i = 0;
+		while (s2[i] != '\0')
+		{
+			s[i + x] = s2[i];
+			i++;
+		}
+		s[i + x] = '\0';
+		return (s);
+	}
+	else
+		return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+char *str_concat(char *s1, char *s2);
 char *_strdup(const char *str);
 char *_strdup(const char *str);
 char *create_array(unsigned int size, char c);
