9da96d03a9c7a027ccf5e11c5d32e2e83f5b31ad
sofi <bedlusofi@gmail.com>
2022-10-04 13:20:12 +0000

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * alloc_grid - return pointer to 2 dimensional array of integer
+ * @width: width of the array(columun)
+ * @height: row
+ * Return: pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = malloc(sizeof(int *) * height);
+	if (p != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			p[i] = malloc(sizeof(int) * width);
+		}
+		i = 0;
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				p[i][j] = 0;
+			}
+		}
+		return (p);
+	}
+	else
+		return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(const char *str);
 char *_strdup(const char *str);
