673b2a6bc44b460c4a7520ea07d08837d30d3879
sofi <bedlusofi@gmail.com>
2022-10-10 15:07:02 +0000

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct dog - define property of dog
@@ -17,5 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
