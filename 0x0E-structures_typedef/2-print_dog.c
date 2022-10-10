09930b2e0c8ce04914ccc5f037ecc0f06bac0205
sofi <bedlusofi@gmail.com>
2022-10-10 17:59:43 +0000

    correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: ",
-		d->name != NULL ? name : "(nil)",
-		d->age, d->owner != NULL ? owner : "(nil)");
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+		d->name != NULL ? d->name : "(nil)",
+		d->age, d->owner != NULL ? d->owner : "(nil)");
 	}
 }
