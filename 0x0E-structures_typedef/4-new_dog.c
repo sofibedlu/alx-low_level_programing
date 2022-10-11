f54762e1471db757e3368428f9f352b3197714ba
sofi <bedlusofi@gmail.com>
2022-10-11 09:20:38 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * new_dog - create new dog
+ * @name: name of a dog
+ * @age: age
+ * @owner: owner
+ * Return: poiter to new object
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t newDog;
+	dog_t *p;
+
+	p = &newDog;
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+
+}
