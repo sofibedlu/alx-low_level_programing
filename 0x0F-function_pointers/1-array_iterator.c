f27ff2620257db91fcc688469660229131649c2e
sofi <bedlusofi@gmail.com>
2022-10-12 19:15:22 +0000

    check

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != NULL)
-	{
-		for (i = 0; i < size; i++)
-			action(array[i]);
-	}
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
