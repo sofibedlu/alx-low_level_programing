62173891b138e4f99a399d02d2a12fa5bb48b92a
sofi <bedlusofi@gmail.com>
2022-10-14 11:47:24 +0000

    print-all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ * printInt - print integer
+ * @args: argument
+ */
+void printInt(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+ * printChar - print char
+ * @args: argument
+ */
+void printChar(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+/**
+ * printFloat - print floats
+ * @args: arguments
+ */
+void printFloat(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * printString - print string
+ * @args: arguments
+ */
+void printString(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+}
+
+/**
+ * print_all - print anything
+ * @format: list of types of arguments
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	size_t i = 0;
+	int j = 0;
+	char *sep = "";
+
+	Selector array[] = {
+		{"i", printInt},
+		{"c", printChar},
+		{"f", printFloat},
+		{"s", printString},
+	};
+	va_start(args, format);
+
+	while (i < strlen(format) && format != NULL)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == array[j].data_type[0])
+			{
+				printf("%s", sep);
+				array[j].fun_type(args);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+#include <stdarg.h>
+struct selector
+{
+	char *data_type;
+	void (*fun_type)(va_list args);
+};
+typedef struct selector Selector;
+void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
