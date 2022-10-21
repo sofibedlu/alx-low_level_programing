9d482a491fbf48bfebf437e5550f84cd5fcf1e36
sofi <bedlusofi@gmail.com>
2022-10-21 08:43:17 +0000

    before

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void beforeFun(void) __attribute__ ((constructor));
+/**
+ * beforeFun - excuted before main function
+ */
+
+void beforeFun(void)
+{
+	printf("you're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
