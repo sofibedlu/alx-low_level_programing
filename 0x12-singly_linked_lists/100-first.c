a2cd3870677c3a9c473daaaed558aaa5de47e0b7
sofi <bedlusofi@gmail.com>
2022-10-21 09:02:23 +0000

    check

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
 void beforeFun(void) __attribute__ ((constructor));
 /**
  * beforeFun - excuted before main function
  */
-
 void beforeFun(void)
 {
 	printf("you're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
+}
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature,"
+			" challenges one to a race.)\n");
+	return (0);
 }
