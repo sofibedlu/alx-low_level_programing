4fdc03e9f503928c68521e50f50d8213bc0e2cef
sofi <bedlusofi@gmail.com>
2022-10-21 09:03:55 +0000

    checking

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,9 +8,3 @@ void beforeFun(void)
 	printf("you're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature,"
-			" challenges one to a race.)\n");
-	return (0);
-}
