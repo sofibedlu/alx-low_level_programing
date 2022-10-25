90ce409c789a629c164629bce86f1995697abadb
sofi <bedlusofi@gmail.com>
2022-10-25 10:50:49 +0000

    correction5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < (idx - 1))
 	{
 		if (temp == NULL)
+		{
+			free(new_node);
 			return (NULL);
+		}
 		temp = temp->next;
 		i++;
 	}
