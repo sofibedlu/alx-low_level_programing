2a7d46f628bda0d8af1cb6184f19d242e922e818
sofi <bedlusofi@gmail.com>
2022-10-25 10:17:51 +0000

    correction1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,6 +20,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (i < index)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 		i++;
 	}
 	return (temp);
