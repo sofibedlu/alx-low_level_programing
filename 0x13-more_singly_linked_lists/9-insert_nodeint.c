aab80fad17b874619f930d3b9e74badfc0edfe7f
sofi <bedlusofi@gmail.com>
2022-10-25 10:34:57 +0000

    correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,14 +21,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
 	temp = *head;
 	temp1 = *head;
 	while (i < idx)
 	{
-		if (temp != NULL)
-			temp = temp->next;
-		else
+		if (temp == NULL)
 			return (NULL);
+		temp = temp->next;
 		i++;
 	}
 	i = 0;
