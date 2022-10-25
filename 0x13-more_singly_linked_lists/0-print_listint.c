9d91dde3bdbc4bb6bcab70fa99f082bac1b3de8f
sofi <bedlusofi@gmail.com>
2022-10-25 08:03:44 +0000

    correction1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,6 @@ size_t print_listint(const listint_t *h)
 	int i = 0;
 	const listint_t *temp;
 
-	if (h == NULL)
-	{
-		printf("Error\n");
-		return (-1);
-	}
 	temp = h;
 	while (temp != NULL)
 	{
