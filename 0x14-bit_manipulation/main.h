a753584cfd8832de3baa460ba15030167e08e4b1
sofi <bedlusofi@gmail.com>
2022-10-27 18:05:51 +0000

    clear

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <math.h>
 #define BYTE_LENGHT 8
 
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
