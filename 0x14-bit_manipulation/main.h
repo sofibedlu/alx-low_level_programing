2b9232130c35ca41b608ae9bbdd8784f283d704f
sofi <bedlusofi@gmail.com>
2022-10-27 17:55:25 +0000

    get bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
