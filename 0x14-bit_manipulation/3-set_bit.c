89e5b05f412ccb672ba781cee585d43739c3e672
sofi <bedlusofi@gmail.com>
2022-10-28 17:52:08 +0000

    testing

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,17 +9,7 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int i = 0;
-	int num = *n;
-	int index1 = index;
-
-	while (num > 0)
-	{
-		num = num >> 1;
-		i++;
-	}
-	i--;
-	if (index1 > i && *n > 0)
+	if (index >=  sizeof(unsigned long int) * BYTE_LENGTH)
 		return (-1);
 	*n |= (1 << index);
 	return (1);
