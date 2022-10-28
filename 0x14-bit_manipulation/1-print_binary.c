d65a9cb4754573c504621db3b9a166cb39294120
sofi <bedlusofi@gmail.com>
2022-10-28 14:27:00 +0000

    final

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Search a string for any of a set of bytes.
- * @x: base
- * @y: exposant
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
- */
-
-unsigned long int _pow_recursion(int x, int y)
-{
-
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
-
-return (x * _pow_recursion(x, y - 1));
-
-}
-
-
-/**
  * print_binary - prints decimal to binary
  * Description: first, finds the biggest exponent for 2, then go down
  * to find the smaller values
@@ -32,21 +9,22 @@ return (x * _pow_recursion(x, y - 1));
 
 void print_binary(unsigned long int n)
 {
-	int i;
+	int bin;
+	unsigned long int binNum = 0, mul = 1;
 
-	for (i = 0; _pow_recursion(2, i) <= n; i++)
-	;
 	if (n == 0)
-		i++;
-
-	for (i--; i >= 0; i--)
+		_putchar('0' + 0);
+	if (n == 1)
+		_putchar('0' + 1);
+	if (n > 1)
 	{
-		if (_pow_recursion(2, i) <= n)
+		while (n > 0)
 		{
-			_putchar('1');
-			n -= _pow_recursion(2, i);
+			bin = n & 1;
+			binNum = binNum + (bin * mul);
+			mul = mul * 10;
+			n = n >> 1;
 		}
-		else
-			_putchar('0');
+		printf("%lu", binNum);
 	}
 }
