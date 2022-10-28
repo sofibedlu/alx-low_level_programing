b8f4e4a39ae7b23e7cb4031bca9a7c4d76707ccd
sofi <bedlusofi@gmail.com>
2022-10-28 16:56:28 +0000

    get-bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Search a string for any of a set of bytes.
- * @x: base
- * @y: exposant
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
- */
-
-unsigned long int _pow_recursion(int x, int y)
-{
-
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
-
-return (x * _pow_recursion(x, y - 1));
-
-}
-
-
-/**
  * get_bit - gets bit on index `index`
  * @n: decimal number
  * @index: index of the bit
@@ -32,28 +9,22 @@ return (x * _pow_recursion(x, y - 1));
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	unsigned int num;
+	int bit, i = 0;
+	int index1 = index;
 
-	for (i = 0; _pow_recursion(2, i) <= n; i++)
-	;
-
-	if (n == 0)
+	num = n;
+	while (n > 0)
+	{
+		n = n >> 1;
 		i++;
-
-	do {
-		i--;
-		if (i == index)
-		{
-			if (_pow_recursion(2, i) <= n)
-				return (1);
-			else
-				return (0);
-		}
-
-		if (_pow_recursion(2, i) <= n)
-			n -= _pow_recursion(2, i);
-
-	} while (i != 0);
-
+	}
+	i--;
+	for (; i >= 0; i--)
+	{
+		bit = (num >> i) & 1;
+		if (i == index1)
+			return (bit);
+	}
 	return (-1);
 }
