deadecd1c1b3d308b224b83674bb39a90df03d27
sofi <bedlusofi@gmail.com>
2022-10-31 19:56:36 +0000

    check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	char *buffer;
+	int fd, i = 0;
 	ssize_t w;
 
 	if (filename == NULL)
@@ -17,17 +16,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	buffer = malloc(strlen(text_content) + 1);
-	if (buffer == NULL)
-		return (-1);
-	strcpy(buffer, text_content);
 	if (text_content != NULL)
 	{
-		w = write(fd, buffer, strlen(buffer) + 1);
+		while (text_content[i] != '\0')
+			i++;
+		w = write(fd, text_content, i);
 		if (w == -1)
 			return (-1);
 	}
 	close(fd);
-	free(buffer);
 	return (1);
 }
