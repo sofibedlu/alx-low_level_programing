bfbabfae121e6e00e86a24cd1ea483d4f23e67d5
sofi <bedlusofi@gmail.com>
2022-10-31 20:37:37 +0000

    check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	fd  = open(filename, O_CREAT | O_APPEND | O_WRONLY, 00664);
 	if (fd == -1)
 		return (-1);
