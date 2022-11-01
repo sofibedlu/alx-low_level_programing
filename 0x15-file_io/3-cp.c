13d1694b307907a1991dc48caf23cc7413fd32ba
sofi <bedlusofi@gmail.com>
2022-11-01 19:24:44 +0000

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define ER STDERR_FILENO
 
 /**
  * main - check-code
@@ -14,43 +15,28 @@ int main(int argc, char **argv)
 	mode_t mode;
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fdr = open(argv[1], O_RDONLY);
 	if (fdr == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
+		dprintf(ER, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fdw = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, mode);
 	if (fdw == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(ER, "Error: Can't write to %s\n", argv[2]), exit(99);
 	r = read(fdr, buffer, 1024);
 	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(ER, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	if (r > 0)
 	{
 		w = write(fdw, buffer, (ssize_t) r);
 		if (w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
-				exit(99);
+			dprintf(ER, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-
 	r = close(fdr);
 	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdr);
-		exit(100);
-	}
+		dprintf(ER, "Error: Can't close fd %d\n", fdr), exit(100);
 	w = close(fdw);
 	if (w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdr);
-		exit(100);
-	}
+		dprintf(ER, "Error: Can't close fd %d\n", fdr), exit(100);
 	return (0);
 }
