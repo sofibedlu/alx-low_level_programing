525669953b2de4874a0a4a3d8335f89eca083355
sofi <bedlusofi@gmail.com>
2023-01-06 15:05:03 +0000

    trying

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (strcmp(key, "") == 0)
 		return (0);
-	new->key = strdup(key);
+	new->key = (char *)key;
 	new->value = strdup(value);
 	new->next = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
