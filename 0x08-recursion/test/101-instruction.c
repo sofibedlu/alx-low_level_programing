27365f97370157e7ac39e0a2244c795e3061f99e
sofi <bedlusofi@gmail.com>
2023-03-08 20:58:16 +0000

    test

---
+++ 0x08-recursion/test/101-instruction.c
@@ -0,0 +1,64 @@
+Write a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.
+
+Prototype: int wildcmp(char *s1, char *s2);
+s2 can contain the special character *.
+The special char * can replace any string (including an empty string)
+
+/*****/
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
+    return (0);
+}
+
+/******/
+
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 101-main.c 101-wildcmp.c -o 101-wildcmp
+julien@ubuntu:~/0x08. Recursion$ ./101-wildcmp
+1
+1
+1
+1
+1
+1
+1
+0
+1
+1
+0
+0
+julien@ubuntu:~/0x08. Recursion$
