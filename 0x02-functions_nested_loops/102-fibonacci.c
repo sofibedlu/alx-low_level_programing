d45f28b342e0ea2af93962f6730545b3551193af
sofi <bedlusofi@gmail.com>
2023-03-09 18:19:50 +0000

    test

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,4 @@
+Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+
+The numbers must be separated by comma, followed by a space , 
+You are allowed to use the standard library
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,7 @@
+Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+
+The numbers should be separated by comma, followed by a space ,
+You are allowed to use the standard library
+You are not allowed to use any other library (You can’t use GMP etc…)
+You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
+You are not allowed to hard code any Fibonacci number (except for 1 and 2)
