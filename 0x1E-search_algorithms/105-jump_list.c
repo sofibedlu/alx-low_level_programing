040aefb65cce84c42fa163f8285bd4b71f8dd946
sofi <bedlusofi@gmail.com>
2023-04-11 22:11:44 +0000

    check for null list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,6 +1,8 @@
 #include <math.h>
 #include "search_algos.h"
 
+listint_t *linear(listint_t *temp, size_t l, size_t r, int value);
+
 /**
  * jump_list - searches for a value in a sorted list of integers
  * using jump search algorithm
@@ -12,8 +14,10 @@
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t l = 0, r = sqrt(size), i;
-	listint_t *temp = list, *prev;
+	listint_t *temp = list, *prev, *result;
 
+	if (list == NULL)
+		return (NULL);
 	while (r < size)
 	{
 		prev = temp;
@@ -43,6 +47,22 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 			break;
 		}
 	}
+	result = linear(temp, l, r, value);
+	return (result);
+}
+
+/**
+ * linear - search for the value in the list in given range
+ * @temp: pointer to current position of the list
+ * @l: lower bound
+ * @r: upper bound
+ * @value: value to search for
+ * Return: pointer to the node the value match or NULL
+ */
+listint_t *linear(listint_t *temp, size_t l, size_t r, int value)
+{
+	size_t i;
+
 	for (i = l; i <= r; i++)
 	{
 		printf("Value checked at index [%ld] = [%d]\n", temp->index, temp->n);
